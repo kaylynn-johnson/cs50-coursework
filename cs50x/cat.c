58ee82bf6e80500cac8dc6a3d5c032dc2830f172
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-02-26 01:51:53 +0000

    Thu, Feb 26, 2026, 1:51 AM +00:00

--- cs50x/cat.c
+++ cs50x/cat.c
@@ -3,7 +3,13 @@
 
 int main(void)
 {
-    int n = get_int("WHat's n? ");
+    int n = get_int("What's n? ");
+
+    while (n < 0)
+    {
+        n = get_int("What's n? ");
+    }
+
     for (int i = 0; i < n; i++)
     {
         printf("meow\n");
