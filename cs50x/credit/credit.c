a22c92705c9f3a0a6cae19b305913d388d3679c4
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-02 03:58:48 +0000

    Sun, Mar 1, 2026, 8:58 PM -07:00

---
+++ cs50x/credit/credit.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //get input from user
+    long credit_num = get_long("What is your credit card number? ");
+
+    // check output from user
+    // call the function
+}
+
+
+str check_output(num)
+{
+    
+}
