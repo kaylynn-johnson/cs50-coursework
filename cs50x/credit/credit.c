6e762c5c9f2e9e435e601755609034715c9dcfe7
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-02 04:11:02 +0000

    Sun, Mar 1, 2026, 9:11 PM -07:00

--- cs50x/credit/credit.c
+++ cs50x/credit/credit.c
@@ -16,7 +16,28 @@ str check_output(num)
 {
     int num_len = strlen(num);
 
-    if (num_len == 13 || num_len == 14) && () {
+    if (num_len == 13 || num_len == 14) && (num[0] == 4) {
         // Is a possible VISA card number
+        return "VISA";
+    }
+    elif (num_len == 15 && (num[0:1] == 34 || num[0:1] == 37)) {
+        // Is a possible AMEX card number
+        return "AMEX";
+    }
+    elif (num_len == 15 && (num[0] == 4)) {
+        // Is a possible VISA card number
+        return "VISA";
+    }
+    elif ((num_len == 16) && (num[0:1] > 50 & num[0:1] < 56)) {
+        // Is a possible MASTERCARD card number
+        return "MASTERCARD";
+    }
+    elif ((num_len == 16) && (num[0] == 4)) {
+        // Is a possible VISA card number
+        return "VISA";
+    }
+    else {
+        // Is an invalid card number
+        return "INVALID";
     }
 }
