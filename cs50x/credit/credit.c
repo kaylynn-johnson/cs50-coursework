87b9021ba29489733c74e086b5dd801bcd655ef9
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-03 00:20:10 +0000

    Mon, Mar 2, 2026, 5:20 PM -07:00

--- cs50x/credit/credit.c
+++ cs50x/credit/credit.c
@@ -3,30 +3,30 @@
 #include <string.h>
 
 string check_output(string str_num, int num_len);
-string calculate_checksum(string str_num, int num_len);
+string determine_valid_checksum(string str_num, int num_len);
 
 int main(void)
 {
-    // get input from user
+    // Get input from user
     long credit_num = get_long("What is your credit card number? ");
+
+    // Create input variables
     char str_num[20];
     sprintf(str_num, "%ld", credit_num);
     int num_len = strlen(str_num);
 
-    // check output from user
-    // call the function
+    // Check output from user
     string card_type = check_output(str_num, num_len);
 
-    // just report invalid if that is the return on check output
+    // Report INVALID if card_type returns incorrect length/initial digits
     if (strcmp(card_type, "INVALID") == 0)
     {
         printf("%s\n", card_type);
         return 0;
     }
 
-    // calculate checksum
-    // call the function
-    string valid_checksum = calculate_checksum(str_num, num_len);
+    // Calculate checksum
+    string valid_checksum = determine_valid_checksum(str_num, num_len);
 
     if (strcmp(valid_checksum, "VALID") == 0)
     {
@@ -69,28 +69,38 @@ string check_output(string str_num, int num_len)
     }
 }
 
-string calculate_checksum(string str_num, int num_len)
+string determine_valid_checksum(string str_num, int num_len)
 {
+    // Initialize part one variables
     int part1_sum = 0;
     int inter_digit;
     char str_double[3];
+
+    // Loop through every other digit starting with second to last
     for (int i = num_len - 2; i > -1; i -= 2)
     {
+        // Calculate the double of every other digit starting with second to last
         inter_digit = (str_num[i] - '0') * 2;
+        // Assign to a string since these digits need to be added together
         sprintf(str_double, "%i", inter_digit);
+        // Keep a running sum of these digits
         for (int j = 0; j < strlen(str_double); j++)
         {
             part1_sum += (str_double[j] - '0');
         }
     }
 
+    // Initialize part two variables
     int part2_sum = part1_sum;
+
+    // Loop through every other digit starting with last
     for (int k = num_len - 1; k > -1; k -= 2)
     {
         inter_digit = str_num[k] - '0';
         part2_sum += inter_digit;
     }
 
+    // Determine if modulo 10 equals 0
     if ((part2_sum % 10) == 0)
     {
         return "VALID";
