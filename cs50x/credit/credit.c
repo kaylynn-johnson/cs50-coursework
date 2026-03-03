e9dcae473b0b3e9e81fe280f295e60f5368e40f6
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-03 00:10:55 +0000

    Mon, Mar 2, 2026, 5:10 PM -07:00

--- cs50x/credit/credit.c
+++ cs50x/credit/credit.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 
-string check_output(long num);
-string calculate_checksum(long num);
+string check_output(char str_num, int num_len);
+string calculate_checksum(char str_num, int num_len);
 
 int main(void)
 {
     // get input from user
     long credit_num = get_long("What is your credit card number? ");
+    char str_num[20];
+    sprintf(str_num, "%ld", credit_num);
+    int num_len = strlen(str_num);
 
     // check output from user
     // call the function
-    string card_type = check_output(credit_num);
-    printf("%s\n", card_type);
+    string card_type = check_output(str_num, num_len);
 
     // just report invalid if that is the return on check output
     if (strcmp(card_type, "INVALID") == 0)
@@ -24,8 +26,7 @@ int main(void)
 
     // calculate checksum
     // call the function
-    string valid_checksum = calculate_checksum(credit_num);
-    // printf("%s\n", valid_checksum);
+    string valid_checksum = calculate_checksum(str_num, num_len);
 
     if (strcmp(valid_checksum, "VALID") == 0)
     {
@@ -39,15 +40,8 @@ int main(void)
     }
 }
 
-string check_output(long num)
+string check_output(char str_num, int num_len)
 {
-    char str_num[20];
-    sprintf(str_num, "%ld", num);
-    // printf("%s\n", str_num);
-    int num_len = strlen(str_num);
-    // printf("%i\n", num_len);
-    // printf("%c\n", str_num[0]);
-
     if ((num_len == 13) && (str_num[0] == '4'))
     {
         // Is a possible VISA card number
@@ -75,17 +69,11 @@ string check_output(long num)
     }
 }
 
-string calculate_checksum(long num)
+string calculate_checksum(char str_num, int num_len)
 {
-    char str_num[20];
-    sprintf(str_num, "%ld", num);
-    //printf("%s\n", str_num);
-    int num_len = strlen(str_num);
-
     int part1_sum = 0;
     int inter_digit;
     char str_double[3];
-    //printf("%i\n", (str_num[0] - '0'));
     for (int i = num_len - 2; i > -1; i -= 2)
     {
         inter_digit = (str_num[i] - '0') * 2;
@@ -94,20 +82,7 @@ string calculate_checksum(long num)
         {
             part1_sum += (str_double[j] - '0');
         }
-        //part1_sum += (inter_digit * 2);
     }
-    //printf("%i\n", part1_sum);
-
-    //int part2_sum = 0;
-    //char str_part1[5];
-    //sprintf(str_part1, "%i", part1_sum);
-    //int part1_len = strlen(str_part1);
-    //for (int j = 0; j < part1_len; j++)
-    //{
-    //    inter_digit = str_part1[j] - '0';
-    //    part2_sum += inter_digit;
-    //}
-    //printf("%i\n", part2_sum);
 
     int part2_sum = part1_sum;
     for (int k = num_len - 1; k > -1; k -= 2)
@@ -115,8 +90,6 @@ string calculate_checksum(long num)
         inter_digit = str_num[k] - '0';
         part2_sum += inter_digit;
     }
-    //printf("%i\n", part2_sum);
-    //printf("%i\n", part3_sum % 10);
 
     if ((part2_sum % 10) == 0)
     {
