b08b245b7835f3ef0dd1e23212852e798c8667bd
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-05 04:24:20 +0000

    Wed, Mar 4, 2026, 9:24 PM -07:00

--- cs50x/lecture2/initials.c
+++ cs50x/lecture2/initials.c
@@ -3,18 +3,16 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 3 || argc == 4)
-    {
-        // User input the correct arguments (either middle name or not)
-        for (int i = 1; i < argc; i++)
-        {
-            printf("%c", argv[i][0]);
-        }
-        printf("\n");
-    }
-    else
+    if (argc < 3)
     {
         // User input is not correct
         printf("Usage: ./initials First Last\n");
+        return 1;
     }
+    // User input the correct arguments (either middle name or not)
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%c", argv[i][0]);
+    }
+    printf("\n");
 }
