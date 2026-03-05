ae30a3a6c7f543ea8b6df942e98b5345837d6884
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-05 04:40:33 +0000

    Wed, Mar 4, 2026, 9:40 PM -07:00

--- cs50x/lecture2/triangle.c
+++ cs50x/lecture2/triangle.c
@@ -5,5 +5,45 @@ bool valid_triangle(int a, int b, int c);
 
 int main(void)
 {
-    
+    int side1 = get_int("Side 1: ");
+    int side2 = get_int("Side 2: ");
+    int side3 = get_int("Side 3: ");
+
+    bool valid = valid_triangle(side1, side2, side3);
+
+    if (valid)
+    {
+        printf("The triangle is valid.\n");
+    }
+    else
+    {
+        printf("The triangle is not valid.\n");
+    }
+
+
+}
+
+bool valid_triangle(int a, int b, int c)
+{
+    if (a < 1 || b < 1 || c < 1)
+    {
+        return false;
+    }
+
+    if ((a + b) < c)
+    {
+        return false;
+    }
+    else if ((b + c) < a)
+    {
+        return false;
+    }
+    else if ((a + c) < b)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
