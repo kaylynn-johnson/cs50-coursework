ad70c16cae99bb63423e9598e93be3100f1da660
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-06 01:58:32 +0000

    Thu, Mar 5, 2026, 6:58 PM -07:00

---
+++ cs50x/scrabble/scrabble.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+char letters[] = {a,}
+
+int main(void)
+{
+    // Get input from two players
+
+    // Loop through lowercase word
+    // Find scrabble score for letter
+    // Add to running sum
+
+    // Compare player1's score vs player2's score
+}
+
+
+int determine_letter_score(char letter)
+{
+
+}
