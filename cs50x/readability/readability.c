71ccf24ea50299a4ec4cb0d5fad513ba3b69003f
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-06 02:29:51 +0000

    Thu, Mar 5, 2026, 7:29 PM -07:00

--- cs50x/readability/readability.c
+++ cs50x/readability/readability.c
@@ -7,5 +7,13 @@ int main(void)
     // Ask for text
 
     // Outer for loop for the 100 word pieces
-    // Inner for loop for counting the 
+    // Inner for loop for counting the letters & sentences per section
+
+    // Add value from inner loop to outer loop
+
+    // Calculate average for both based on number of times the outer loop ran
+
+    // Run the equation
+
+    // Print out the appropriate grade level
 }
