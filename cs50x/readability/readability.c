7c2e2e69e1984243eaeec8ed1d5965f5ea98bcfc
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-06 02:46:01 +0000

    Thu, Mar 5, 2026, 7:46 PM -07:00

--- cs50x/readability/readability.c
+++ cs50x/readability/readability.c
@@ -1,21 +1,77 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     // Ask for text
+    string text = get_string("Text: ");
 
-    // Outer for loop for the 100 word pieces
-    // Inner for loop for counting the letters & sentences per section
+    // Calculate number of words in the text (1)
+    int num_words = calculate_num_words(text);
 
-    // Add value from inner loop to outer loop
+    // Calculate number of letters in the text (2)
+    int num_letters = calculate_num_letters(text);
 
-    // Calculate average for both based on number of times the outer loop ran
+    // Calculate number of sentences in the text (3)
+    int num_sentences = calculate_num_sentences(text);
 
-    // Run the equation
+    // Calculate L = (2) / (1) * 100
+    float L = (num_letters / num_words) * 100.0
+
+    // Calculate S = (3) / (1) * 100
+    flow
+
+    // Run the equation (0.0588 * L) - (0.296 * S) - 15.8
 
     // Print out the appropriate grade level
 
     // How would we handle the last bit that is less than 100 words?
+}
+
+int calculate_num_words(string text)
+{
+    // increment counter when see a " " (space) or ASCII 32
+    int num_words = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == 32) {
+            num_words++
+        }
+    }
+
+    return num_words;
+}
+
+int calculate_num_letters(string text)
+{
+    // increment counter when see a letter (between 97-122) after lowering text
+    int num_letters = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (tolower(text[i]) >= 97 && tolower(text[i]) <- 122)
+        {
+            // This is a letter
+            num_letters++;
+        }
+    }
+
+    return num_letters;
+}
+
+int calculate_num_sentences(string text)
+{
+    // increment counter when see a ".", "!", or "?" or ASCII 33, 46, 63
+    int num_sentences = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == 33 || text[i] == 46 || text[i] == 63)
+        {
+            // There is an end of sentence punctuation
+            num_sentences++;
+        }
+    }
+
+    return num_sentences;
 }
