ddd2d5538818b8d37242d2ab36e71bb68db07186
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-06 03:28:50 +0000

    Thu, Mar 5, 2026, 8:28 PM -07:00

--- cs50x/substitution/substitution.c
+++ cs50x/substitution/substitution.c
@@ -1,2 +1,11 @@
 #include <cs50.h>
-#include <
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    // check for valid number of arguments
+
+    // check for valid key
+    // length must be 26 (magic number)
+    // each letter should only be used once (move forwards and backwards)
+}
