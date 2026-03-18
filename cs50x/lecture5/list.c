d154b7155a2abc56defe9a5c000a62e53b21d38d
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-18 02:18:39 +0000

    Tue, Mar 17, 2026, 8:18 PM -06:00

--- cs50x/lecture5/list.c
+++ cs50x/lecture5/list.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int list[3];
-
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
-    for (int i = 0; i < 3; i++)
+    // Need another bit of memory
+    int *temp = realloc(list, 4 * sizeof(int));
+    if (temp == NULL)
     {
-        printf("%i\n", i);
+        free(list);
+        return 1;
     }
+    temp[3] = 4;
+
+    free(list);
+    list = temp;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
+    }
+
+    free(list);
 }
