3e0f462fb8504ee1fb846e4d7ab14da5d9441d62
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-27 22:46:24 +0000

    Fri, Mar 27, 2026, 4:46 PM -06:00

--- cs50x/lecture5/hash.c
+++ cs50x/lecture5/hash.c
@@ -6,7 +6,7 @@ int main(void)
 {
     char *input = get_string("Word: ");
 
-    int hash_num = input[0].toupper() - 'A';
+    int hash_num = toupper(input[0]) - 'A';
 
     printf("The hash number is %i\n", hash_num);
 }
