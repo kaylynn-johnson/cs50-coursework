d2c8e1ac89c9569d58caab8b4ec4f6c2b3b9e645
Kaylynn Johnson <kaylynnrogers3@gmail.com>
2026-03-29 01:06:41 +0000

    Sat, Mar 28, 2026, 7:06 PM -06:00

--- cs50x/inheritance/inheritance.c
+++ cs50x/inheritance/inheritance.c
@@ -82,7 +82,7 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL)
+    if (p == NULL)
     {
         return;
     }
