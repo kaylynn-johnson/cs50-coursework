69026be12b40b752fcee90f201070194e796c0e5
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-07-14 23:05:30 -0600

    add behavior for viewing a listing, placing a bid, adding a comment and start of adding listing to wishlist

--- cs50w/commerce/auctions/urls.py
+++ cs50w/commerce/auctions/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "auctions"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("create/", views.create_listing, name="create")
+    path("create/", views.create_listing, name="create"),
+    path("<str:listing_id>", views.listing, name="listing"),
+    path("<str:listing_id>/bid", views.place_bid, name="bid_path"),
+    path("<str:listing_id>/comment", views.comment, name="comment"),
+    path("wishlist/", views.add_to_wishlist, name="wishlist")
 ]
--- cs50w/commerce/auctions/views.py
+++ cs50w/commerce/auctions/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
-from django.db import IntegrityError
+from django.db import IntegrityError, models
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
 
-from .models import User, Bid, Comment, AuctionListing
+from .models import User, Bid, Comment, AuctionListing, Wishlist
 
 class NewListingForm(forms.ModelForm):
     class Meta:
@@ -25,7 +25,7 @@ class NewCommentForm(forms.ModelForm):
 def index(request):
     # determine all of the auction listings that are active
     return render(request, "auctions/index.html", {
-        "active_listings": AuctionListing.objects.filter(active__exact=True)
+        "active_listings": AuctionListing.objects.filter(active__exact=True).annotate(max_price=models.Max('bids__price'))
     })
 
 
@@ -90,7 +90,7 @@ def create_listing(request):
             new_listing.owner = request.user
             new_listing.save()
             # redirect to index
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("auctions:index"))
         
         #form is not valid
         return render(request, "auctions/create.html", {
@@ -100,4 +100,77 @@ def create_listing(request):
     # user got here by GET
     return render(request, "auctions/create.html", {
         "form": NewListingForm()
+    })
+
+
+def place_bid(request, listing_id):
+    if request.method == "POST":
+        # user submitted the form for the bid
+        form = NewBidForm(request.POST)
+        if form.is_valid():
+            # determine if bid is high enough
+            bids_all = AuctionListing.objects.get(pk=listing_id).bids.all()
+            if not bids_all:
+                # no bids found so compare against starting price
+                price_to_beat = AuctionListing.objects.get(pk=listing_id).starting_price
+            else:
+                price_to_beat = bids_all.aggregate(max_price=models.Max('price'))['max_price']
+            
+            # check with what user put in
+            if int(request.POST["price"]) > price_to_beat:
+                new_bid = form.save(commit=False)
+                new_bid.user = request.user
+                new_bid.listing = AuctionListing.objects.get(pk=listing_id)
+                new_bid.save()
+                error = ""
+            else:
+                # show error
+                error = "Bid is not high enough"
+    
+    #user got here for GET
+    return HttpResponseRedirect(reverse("auctions:listing", args=(listing_id,)))
+
+
+def listing(request, listing_id):
+    # give all the auction listing information to the page
+    # find appropriate price
+    bids_all = AuctionListing.objects.get(pk=listing_id).bids.all()
+    if not bids_all:
+        # no bids
+        price = AuctionListing.objects.get(pk=listing_id).starting_price
+    else:
+        price = bids_all.aggregate(max_price=models.Max('price'))['max_price']
+    return render(request, "auctions/listing.html", {
+        "listing_info": AuctionListing.objects.get(pk=listing_id),
+        "price": price,
+        "bids": AuctionListing.objects.get(pk=listing_id).bids.all(),
+        "bid_form": NewBidForm(),
+        "error": "",
+        "comments": AuctionListing.objects.get(pk=listing_id).comments.all(),
+        "comment_form": NewCommentForm()
+    })
+
+
+def comment(request, listing_id):
+    if request.method == "POST":
+        # user submitted the form for the comment
+        form = NewCommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.listing = AuctionListing.objects.get(pk=listing_id)
+            new_comment.save()
+
+    # redirect back to listing page
+    return HttpResponseRedirect(reverse("auctions:listing", args=(listing_id,)))
+        
+
+def add_to_wishlist(request, listing_id):
+    if request.method == "POST":
+        # no form needed for this as it is just a button
+        wishlist_entry = Wishlist.objects.create(listing=AuctionListing.objects.get(pk=listing_id), user=request.user)
+        wishlist_entry.save()
+
+    return render(request, "auctions/wishlist.html", {
+        "wishlist": User.objects.get(pk=request.user.id).wishlist
     })
