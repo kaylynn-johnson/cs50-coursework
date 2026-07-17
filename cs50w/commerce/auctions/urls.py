cd07b1c6e64ea05c7d35801cd0486ad1d11c1c33
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-07-16 21:04:42 -0600

    add categories page/function

--- cs50w/commerce/auctions/urls.py
+++ cs50w/commerce/auctions/urls.py
@@ -12,7 +12,10 @@ urlpatterns = [
     path("wishlist/", views.user_wishlist, name="wishlist"),
     path("<str:listing_id>", views.listing, name="listing"),
     path("<str:listing_id>/bid", views.place_bid, name="bid"),
+    path("<str:listing_id>/close_bid", views.close_bid, name="close_bid"),
     path("<str:listing_id>/comment", views.comment, name="comment"),
     path("<str:listing_id>/add_wishlist", views.add_to_wishlist, name="add_wishlist"),
-    path("<str:listing_id>/remove_wishlist", views.remove_from_wishlist, name="remove_wishlist")    
+    path("<str:listing_id>/remove_wishlist", views.remove_from_wishlist, name="remove_wishlist"),
+    path("categories/", views.overall_categories, name="categories"),
+    path("categories/<str:category>", views.category_listing, name="category_listing")
 ]
--- cs50w/commerce/auctions/views.py
+++ cs50w/commerce/auctions/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError, models
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
@@ -40,7 +41,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("auctions:index"))
         else:
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
@@ -51,7 +52,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("auctions:index"))
 
 
 def register(request):
@@ -76,11 +77,12 @@ def register(request):
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("auctions:index"))
     else:
         return render(request, "auctions/register.html")
 
 
+@login_required
 def create_listing(request):
     if request.method == "POST":
         # user submitted the form for the listing
@@ -103,16 +105,18 @@ def create_listing(request):
     })
 
 
+@login_required
 def place_bid(request, listing_id):
     if request.method == "POST":
         # user submitted the form for the bid
         form = NewBidForm(request.POST)
         if form.is_valid():
             # determine if bid is high enough
-            bids_all = AuctionListing.objects.get(pk=listing_id).bids.all()
+            listing = AuctionListing.objects.get(pk=listing_id)
+            bids_all = listing.bids.all()
             if not bids_all:
                 # no bids found so compare against starting price
-                price_to_beat = AuctionListing.objects.get(pk=listing_id).starting_price
+                price_to_beat = listing.starting_price
             else:
                 price_to_beat = bids_all.aggregate(max_price=models.Max('price'))['max_price']
             
@@ -120,12 +124,12 @@ def place_bid(request, listing_id):
             if int(request.POST["price"]) > price_to_beat:
                 new_bid = form.save(commit=False)
                 new_bid.user = request.user
-                new_bid.listing = AuctionListing.objects.get(pk=listing_id)
+                new_bid.listing = listing
                 new_bid.save()
             else:
                 # show error
                 return render(request, "auctions/error.html", {
-                    "error": f"Bid for {AuctionListing.objects.get(pk=listing_id).title} is not high enough",
+                    "error": f"Bid for {listing.title} is not high enough",
                     "listing_id": listing_id
                 })
     
@@ -133,31 +137,57 @@ def place_bid(request, listing_id):
     return HttpResponseRedirect(reverse("auctions:listing", args=(listing_id,)))
 
 
+@login_required
+def close_bid(request, listing_id):
+    if request.method == "POST":
+        # owner hit the submit button
+        listing = AuctionListing.objects.get(pk=listing_id)
+        listing.active = False
+        listing.save()
+
+    return HttpResponseRedirect(reverse("auctions:listing", args=(listing_id,)))
+
+
+@login_required
 def listing(request, listing_id):
     # give all the auction listing information to the page
     # find appropriate price
-    bids_all = AuctionListing.objects.get(pk=listing_id).bids.all()
+    listing = AuctionListing.objects.get(pk=listing_id)
+    bids_all = listing.bids.all()
     if not bids_all:
         # no bids
-        price = AuctionListing.objects.get(pk=listing_id).starting_price
+        price = listing.starting_price
+        winning_bidder = None
     else:
         price = bids_all.aggregate(max_price=models.Max('price'))['max_price']
+        winning_bidder = Bid.objects.get(listing=listing, price=price).user
 
     # determine if item is on wishlist
-    on_wishlist = Wishlist.objects.filter(user=request.user, listing=AuctionListing.objects.get(pk=listing_id)).exists()
+    on_wishlist = Wishlist.objects.filter(user=request.user, listing=listing).exists()
+
+    # determine if owner of the listing
+    owner = AuctionListing.objects.filter(pk=listing_id, owner=request.user).exists()
+
+    # determine if logged in user is the same as winning_bidder
+    print(winning_bidder)
+    if winning_bidder == request.user:
+        winner = True
+    else:
+        winner = False
 
     return render(request, "auctions/listing.html", {
-        "listing_info": AuctionListing.objects.get(pk=listing_id),
+        "listing_info": listing,
         "price": price,
-        "bids": AuctionListing.objects.get(pk=listing_id).bids.all(),
+        "owner": owner,
+        "winner": winner,
+        "bids": listing.bids.all(),
         "bid_form": NewBidForm(),
-        "error": "",
-        "comments": AuctionListing.objects.get(pk=listing_id).comments.all(),
+        "comments": listing.comments.all(),
         "comment_form": NewCommentForm(),
         "on_wishlist": on_wishlist
     })
 
-
+@login_required
 def comment(request, listing_id):
     if request.method == "POST":
         # user submitted the form for the comment
@@ -171,7 +201,7 @@ def comment(request, listing_id):
     # redirect back to listing page
     return HttpResponseRedirect(reverse("auctions:listing", args=(listing_id,)))
 
-
+@login_required
 def user_wishlist(request):
     # can get here by GET
     return render(request, "auctions/wishlist.html", {
@@ -179,6 +209,7 @@ def user_wishlist(request):
         "wishlist": Wishlist.objects.filter(user=request.user)
     })
 
+@login_required
 def add_to_wishlist(request, listing_id):
     if request.method == "POST":
         # no form needed for this as it is just a button
@@ -187,9 +218,26 @@ def add_to_wishlist(request, listing_id):
 
     return HttpResponseRedirect(reverse("auctions:wishlist"))
 
+@login_required
 def remove_from_wishlist(request, listing_id):
     if request.method == "POST":
         wishlist_entry = Wishlist.objects.filter(user=request.user, listing=AuctionListing.objects.get(pk=listing_id))
         wishlist_entry.delete()
 
     return HttpResponseRedirect(reverse("auctions:wishlist"))
+
+@login_required
+def overall_categories(request):
+    CATEGORIES = ["Fashion", "Toys", "Electronics",  "Home", "Miscellaneous"]
+    return render(request, "auctions/overall_categories.html", {
+        "categories": CATEGORIES
+    })
+
+@login_required
+def category_listing(request, category):
+    CATEGORIES = {"Fashion": "FASH", "Toys": "TOYS", "Electronics": "ELEC",  "Home": "HOME", "Miscellaneous": "MISC"}
+    listings = AuctionListing.objects.filter(category=CATEGORIES[category], active=True)
+
+    return render(request, "auctions/category.html", {
+        "listings": listings
+    })
