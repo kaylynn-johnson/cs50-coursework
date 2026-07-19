5e2574e5a22540172c4363791adfce0915f52caa
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-07-19 16:32:05 -0600

    Creation of models for posts, likes & followers; giving admin access to those models & migrating

--- cs50w/network/network/models.py
+++ cs50w/network/network/models.py
@@ -4,3 +4,29 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
+    
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} likes post #{self.post.id}"
+    
+
+class Followers(models.Model):
+    initiator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+
+    def __str__(self):
+        return f"{self.initiator.username} follows {self.receiver.username}"
