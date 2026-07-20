c0e4eb404a39d0721f8d668231ebcac6e5981ad9
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-07-20 06:14:15 -0600

    Implement backend POST api for creating post

--- cs50w/network/network/views.py
+++ cs50w/network/network/views.py
@@ -1,10 +1,11 @@
+import json
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Likes, Followers
 
 
 def index(request):
@@ -65,7 +66,26 @@ def register(request):
 
 def create_post(request):
     """Create post for user"""
-    pass
+
+    # Must use POST to create a new post
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required."}, status=400)
+    
+    data = json.loads(request.body)
+    content = data.get("content")
+    if not content:
+        # No content passed
+        return JsonResponse({"error": "Post cannot be blank."}, status=400)
+    
+    # Determine author
+    author = User.objects.get(id=request.id)
+
+    # Create the Post object
+    # Created at and Updated at should be added automatically
+    post = Post(author=author, content=content)
+    post.save()
+
+    return JsonResponse({"message": "Post submitted successfully"}, status=201)
 
 
 def edit_post(request, post_id):
